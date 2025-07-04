b823ba49656b6de765c40a26c8d0e0519ea75863
Fatima Bani Sakher <fatima.mbk00@gmail.com>
2025-07-04 18:50:53 +0300

    update

--- src/firebase.js
+++ src/firebase.js
@@ -6,7 +6,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDbJo27UoyqBcrWyyXpQqla3djOqGLKkIM",
+apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "ai-form-validator.firebaseapp.com",
   projectId: "ai-form-validator",
   storageBucket: "ai-form-validator.firebasestorage.app",
