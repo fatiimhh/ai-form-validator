486cdb497f7a40b5eb159b52b1e8eb49876737d6
Fatima Bani Sakher <fatima.mbk00@gmail.com>
2025-07-08 18:46:05 +0300

    firebase update

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "ai-form-validator.firebaseapp.com",
-  projectId: "ai-form-validator",
-  storageBucket: "ai-form-validator.firebasestorage.app",
-  messagingSenderId: "188650890581",
-  appId: "1:188650890581:web:b3fdba221e50780935bbdc"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
